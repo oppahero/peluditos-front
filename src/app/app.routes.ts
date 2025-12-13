f7b4ad9fb7e39bf2c01a779cd2bddc2a7aa08975
Maria <beadleshero@gmail.com>
2025-12-12 21:55:10 -0400

    feat: add routing for authentication login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,8 @@ export const routes: Routes = [
     path: '',
     loadComponent: () => import('./landing/landing').then((m) => m.Landing),
   },
+  {
+    path: 'auth',
+    loadComponent: () => import('./auth/login/login').then((m) => m.Login),
+  },
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,10 @@
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { PrimeNG } from 'primeng/config';
-import { TableModule } from 'primeng/table';
-import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TableModule, ButtonModule],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
@@ -18,10 +16,4 @@ export class App {
   ngOnInit() {
     this.primeng.ripple.set(true);
   }
-
-  products = [
-    { id: 1, name: 'Product 1', price: 10 },
-    { id: 2, name: 'Product 2', price: 20 },
-    { id: 3, name: 'Product 3', price: 30 },
-  ];
 }
