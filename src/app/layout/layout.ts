5e95a9d1755a24d58a23c4fc4af420aa99599f05
Maria <beadleshero@gmail.com>
2026-01-02 18:23:04 -0400

    feat(layout): update layout structure to include sidebar and enhance topbar semantics

--- src/app/layout/layout.ts
+++ src/app/layout/layout.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Topbar } from './components/topbar/topbar';
+import { Sidebar } from './components/sidebar/sidebar';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  imports: [Topbar],
+  imports: [Topbar, Sidebar, RouterOutlet],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
