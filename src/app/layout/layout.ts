1e894560dadca4dda5b724d4c6479671deeffabc
Maria <beadleshero@gmail.com>
2026-01-06 20:48:47 -0400

    feat(layout): implement sidebar visibility management in layout component

--- src/app/layout/layout.ts
+++ src/app/layout/layout.ts
@@ -1,13 +1,25 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
 import { Topbar } from './components/topbar/topbar';
 import { Sidebar } from './components/sidebar/sidebar';
 import { RouterOutlet } from '@angular/router';
+import { SidebarService } from './services/sidebar-service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-layout',
-  imports: [Topbar, Sidebar, RouterOutlet],
+  imports: [Topbar, Sidebar, RouterOutlet, CommonModule],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Layout {}
+export class Layout implements OnInit {
+  isSidebarVisible!: boolean;
+
+  private sidebarService = inject(SidebarService);
+
+  ngOnInit(): void {
+    this.sidebarService.visible$.subscribe((value) => {
+      this.isSidebarVisible = value;
+    });
+  }
+}
