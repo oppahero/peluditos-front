1fe504d81d446ea241714d4659dbd326e06f6447
Maria <beadleshero@gmail.com>
2026-01-09 00:13:44 -0400

    feat(breeds): add breeds component with template, styles, and tests

---
+++ src/app/features/animals/breeds/components/breeds.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-breeds',
+  imports: [],
+  templateUrl: './breeds.html',
+  styleUrl: './breeds.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Breeds {
+
+}
