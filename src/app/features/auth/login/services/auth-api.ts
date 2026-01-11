db342ddc2dd155914d10c5b9059af667c2bd8273
Maria <beadleshero@gmail.com>
2026-01-11 12:09:23 -0400

    refactor(auth-api): enhance auth methods to include withCredentials option for improved security

--- src/app/features/auth/login/services/auth-api.ts
+++ src/app/features/auth/login/services/auth-api.ts
@@ -1,9 +1,10 @@
 import { LoginCredentials } from '../interfaces/login-credentials.interface';
 import { AuthResponse } from '@app/core/interfaces/auth-response.interface';
 import { environment } from '@environments/environment.development';
-import { catchError, Observable, throwError } from 'rxjs';
+import { catchError, Observable, tap, throwError } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { TokenService } from '@app/core/services';
 
 @Injectable({
   providedIn: 'root',
@@ -11,16 +12,21 @@ import { inject, Injectable } from '@angular/core';
 export class AuthApi {
   private url = environment.apiUrl + '/auth';
   private http = inject(HttpClient);
+  private tokenService = inject(TokenService);
 
   auth(credentials: LoginCredentials): Observable<AuthResponse> {
-    return this.http.post<AuthResponse>(`${this.url}/login`, credentials).pipe(
-      catchError((err) => {
-        return throwError(() => new Error(err?.detail ?? 'Error inesperado'));
-      })
-    );
+    return this.http
+      .post<AuthResponse>(`${this.url}/login`, credentials, { withCredentials: true })
+      .pipe(
+        catchError((err) => {
+          return throwError(() => new Error(err?.detail ?? 'Error inesperado'));
+        })
+      );
   }
 
   refreshToken() {
-    return this.http.post(`${this.url}/refresh`, null);
+    return this.http
+      .post<{ accessToken: string }>(`${this.url}/refresh`, null, { withCredentials: true })
+      .pipe(tap((res) => this.tokenService.setAccessToken(res.accessToken)));
   }
 }
