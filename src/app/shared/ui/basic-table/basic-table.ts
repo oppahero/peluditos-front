1c2ec30bb52f078c256fa511c2d879a094a3b271
Maria <beadleshero@gmail.com>
2026-01-11 00:23:36 -0400

    feat(basic-table): enable custom body template and improve imports

--- src/app/shared/ui/basic-table/basic-table.ts
+++ src/app/shared/ui/basic-table/basic-table.ts
@@ -1,13 +1,20 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ContentChild,
+  input,
+  TemplateRef,
+} from '@angular/core';
 import { Column } from '@app/core/interfaces/primeng.interface';
 import { CommonModule } from '@angular/common';
 import { TableModule } from 'primeng/table';
 import { IconFieldModule } from 'primeng/iconfield';
 import { InputIconModule } from 'primeng/inputicon';
+import { InputText } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-basic-table',
-  imports: [TableModule, CommonModule, IconFieldModule, InputIconModule],
+  imports: [TableModule, CommonModule, IconFieldModule, InputIconModule, InputText],
   templateUrl: './basic-table.html',
   styleUrl: './basic-table.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -17,4 +24,6 @@ export class BasicTable {
   cols = input<Column[]>();
   paginator = input<boolean>(false);
   title = input<string>();
+
+  @ContentChild('body', { static: false }) customBodyTemplate!: TemplateRef<any>;
 }
