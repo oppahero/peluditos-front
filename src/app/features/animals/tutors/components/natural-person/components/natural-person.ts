43ef97b6dd562ff09ac056994830641d912e1ccd
Maria <beadleshero@gmail.com>
2026-01-11 00:24:55 -0400

    refactor(natural-person): update Column interface to make fields optional and adjust column definitions

--- src/app/features/animals/tutors/components/natural-person/components/natural-person.ts
+++ src/app/features/animals/tutors/components/natural-person/components/natural-person.ts
@@ -1,7 +1,8 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
 import { Column } from '@app/core/interfaces/primeng.interface';
 import { BasicTable } from '@app/shared/ui/basic-table/basic-table';
 import { NaturalAndPerson } from '../interfaces/naturalPerson';
+import { NaturalPersonFacade } from '../services/natural-person-facade';
 
 @Component({
   selector: 'app-natural-person',
@@ -10,19 +11,22 @@ import { NaturalAndPerson } from '../interfaces/naturalPerson';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NaturalPerson implements OnInit {
-  rows!: NaturalAndPerson[];
   cols!: Column[];
+
+  private naturalPersonFacade = inject(NaturalPersonFacade);
+  rows = this.naturalPersonFacade.natural;
 
   ngOnInit(): void {
     this.setCols();
+    this.naturalPersonFacade.loadPersons({ page: 1, limit: 10 });
   }
 
   setCols() {
     this.cols = [
-      { field: 'dni', header: 'CI' },
-      { field: 'name', header: 'Nombre' },
-      { field: 'phone', header: 'Teléfono' },
-      { field: 'email', header: 'Correo' },
+      { header: 'CI' },
+      { header: 'Nombre' },
+      { header: 'Teléfono' },
+      { header: 'Correo' },
     ];
   }
 }
