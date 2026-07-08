93cf5b380142a99a58c06cedb543d93f24823e31
Maria <beadleshero@gmail.com>
2026-07-07 22:43:41 -0400

    feat: add new properties to the table

--- src/app/shared/ui/basic-table/basic-table.ts
+++ src/app/shared/ui/basic-table/basic-table.ts
@@ -1,29 +1,34 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  ContentChild,
-  input,
-  TemplateRef,
-} from '@angular/core';
 import { Column } from '@app/core/interfaces/primeng.interface';
-import { CommonModule } from '@angular/common';
-import { TableModule } from 'primeng/table';
 import { IconFieldModule } from 'primeng/iconfield';
 import { InputIconModule } from 'primeng/inputicon';
+import { CommonModule } from '@angular/common';
 import { InputText } from 'primeng/inputtext';
+import { TableModule } from 'primeng/table';
+import {
+  input,
+  Component,
+  ContentChild,
+  TemplateRef,
+  ChangeDetectionStrategy,
+  model,
+} from '@angular/core';
 
 @Component({
   selector: 'app-basic-table',
   imports: [TableModule, CommonModule, IconFieldModule, InputIconModule, InputText],
   templateUrl: './basic-table.html',
-  styleUrl: './basic-table.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BasicTable {
   rows = input<any[]>();
   cols = input<Column[]>();
+  selection = model<any>();
   paginator = input<boolean>(false);
+  scrollable = input<boolean>(true);
+  scrollHeight = input<string>('350px');
+  numOfRows = input<number>(10);
   title = input<string>();
+  selectionMode = input<'single' | 'multiple' | undefined>('single');
   globalFilterFields = input<string[] | undefined>(undefined);
   @ContentChild('body', { static: false }) customBodyTemplate!: TemplateRef<any>;
 }
