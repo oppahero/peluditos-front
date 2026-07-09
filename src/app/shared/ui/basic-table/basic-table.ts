82692038cc407b62cc7846805719eead0320f971
Maria <beadleshero@gmail.com>
2026-07-08 20:43:32 -0400

    feat(basic-table): add summary section for handling pagination and empty message

--- src/app/shared/ui/basic-table/basic-table.ts
+++ src/app/shared/ui/basic-table/basic-table.ts
@@ -2,20 +2,31 @@ import { Column } from '@app/core/interfaces/primeng.interface';
 import { IconFieldModule } from 'primeng/iconfield';
 import { InputIconModule } from 'primeng/inputicon';
 import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
 import { InputText } from 'primeng/inputtext';
 import { TableModule } from 'primeng/table';
 import {
   input,
+  model,
   Component,
   ContentChild,
   TemplateRef,
   ChangeDetectionStrategy,
-  model,
+  output,
 } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-table',
-  imports: [TableModule, CommonModule, IconFieldModule, InputIconModule, InputText],
+  imports: [
+    TableModule,
+    CommonModule,
+    IconFieldModule,
+    InputIconModule,
+    InputText,
+    ButtonModule,
+    FormsModule,
+  ],
   templateUrl: './basic-table.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -23,12 +34,38 @@ export class BasicTable {
   rows = input<any[]>();
   cols = input<Column[]>();
   selection = model<any>();
+  pageInfo = model<any>();
   paginator = input<boolean>(false);
   scrollable = input<boolean>(true);
-  scrollHeight = input<string>('350px');
+  scrollHeight = input<string>('370px');
   numOfRows = input<number>(10);
   title = input<string>();
   selectionMode = input<'single' | 'multiple' | undefined>('single');
   globalFilterFields = input<string[] | undefined>(undefined);
+
   @ContentChild('body', { static: false }) customBodyTemplate!: TemplateRef<any>;
+
+  nextPage = output();
+  prevPage = output();
+  resetPage = output();
+
+  isFirstPage() {
+    return this.pageInfo()?.page === 1;
+  }
+
+  reset() {
+    this.resetPage.emit();
+  }
+
+  prev() {
+    this.prevPage.emit();
+  }
+
+  next() {
+    this.nextPage.emit();
+  }
+
+  isLastPage() {
+    return this.pageInfo()?.page === this.pageInfo()?.lastPage;
+  }
 }
