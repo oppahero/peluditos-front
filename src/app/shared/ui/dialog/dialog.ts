58126623f6ae29b401a7985f0d8a691f0e03dc2b
Maria <beadleshero@gmail.com>
2026-08-30 21:04:50 -0400

    feat(dialog): add close output event

--- src/app/shared/ui/dialog/dialog.ts
+++ src/app/shared/ui/dialog/dialog.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, input, model } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, model, output } from '@angular/core';
 import { PositionType } from '@app/core/interfaces/primeng.interface';
 import { DialogModule } from 'primeng/dialog';
 
@@ -16,4 +16,10 @@ export class Dialog {
   style = input<any>({ width: '50vw' });
   breakpoints = input<any>({ '960px': '75vw', '640px': '90vw', '480px': '100vw' });
   position = input<PositionType>('center');
+
+  readonly closed = output<void>();
+
+  handleClose() {
+    this.closed.emit();
+  }
 }
